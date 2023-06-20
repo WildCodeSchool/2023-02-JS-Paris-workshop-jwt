81ec5ba574246fa1c9918ed33533068a7987f289
ornel77 <t.ornel@yahoo.fr>
2023-06-20 10:57:27 +0200

    Frontend 1 - Login form OK

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,37 @@
+import { useState } from 'react';
+
 function Login() {
-  return <form />;
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if(!email || !password) {
+      alert('Please specify both email and password')
+    } else {
+      console.log(email, password);
+    }
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type='email'
+        name='email'
+        placeholder='Email'
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <br />
+      <input
+        type='password'
+        name='password'
+        placeholder='Password'
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <br />
+      <input type='submit' value='Login' />
+    </form>
+  );
 }
 
 export default Login;
